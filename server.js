4eca57da4455b62cb155814d486480ad79bd55e8
politiviral <mklegacygroupltd@gmail.com>
2026-09-01 14:57:49 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve frontend files
+app.use(express.static(path.join(__dirname)));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/api/health", (req, res) => {
+  res.json({
+    success: true,
+    message: "POLITIVIRAL server is running"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`POLITIVIRAL running on port ${PORT}`);
+});
